b817bd977d38b4b40750d2af804330cc08346720
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2024-01-11 19:25:30 -0500

    signup page work
    added funtionality

--- client-side/src/pages/SignUp.jsx
+++ client-side/src/pages/SignUp.jsx
@@ -1,15 +1,57 @@
-import { Link } from "react-router-dom"
-import { Button, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+
 
 
 
 export default function SignUp() {
+
+  
+  const [formData, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState('')
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+
+  // track the input fields in state
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  // handle the form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: { 'Content-Type': 'application/json' }
+      });
+      const data = await res.json(); 
+      if (data.success === false){
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    }catch(error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+
+    }
+  };  
+
   return (
 
     <div className="min-h-screen mt-20">
-
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-
         {/* left */}
         <div className="flex-1">
           <Link to="/"
@@ -22,43 +64,46 @@ export default function SignUp() {
           <p className="text-sm mt-5">
             These are my thoughts on the world. Want to disagree with me? Sign up to post. 
           </p>
-
         </div>
 
         {/* right */}
-        <div className="flex-1">   
+        <div className="flex-1">  
 
-          <form className="flex flex-col gap-4">
-
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your Username" />
               <TextInput
                 type="text"
                 placeholder="Username"
-                id="username"                
+                id="username"
+                onChange={handleChange}
               />
             </div>
-
             <div>
               <Label value="Your Email" />
               <TextInput
-                type="text"
+                type="email"
                 placeholder="Email"
-                id="email"                
+                id="email" 
+                onChange={handleChange}
               />
             </div>
-
             <div>
               <Label value="Your Password" />
               <TextInput
                 type="password"
                 placeholder="Password"
-                id="password"                
+                id="password" 
+                onChange={handleChange}
               />
             </div>
-
-            <Button gradientDuoTone='purpleToPink' type="submit">
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size={'sm'} />
+                  <span className="pl-3">Loading...</span>
+                </>                
+              ) : 'Sign Up'}
             </Button>
           </form>
 
@@ -67,6 +112,15 @@ export default function SignUp() {
             <Link to="/sign-in" className="text-blue-500 hover:text-blue-700">
               Sign In </Link>
           </div>
+
+          {
+            errorMessage && (
+              <Alert className="mt-5" color="failure">
+                {errorMessage}
+              </Alert>
+            )
+          }
+
 
 
 
--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
+  // this is the path to the client-side code
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
+
   plugins: [react()],
 })
