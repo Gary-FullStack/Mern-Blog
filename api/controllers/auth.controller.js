98b3d8c7a9c2407a1b8198a2372e9a48f7cea26d
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2024-01-17 12:58:45 -0500

    getting google auth
    updating sign in options

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === "" || password === "") {
-   return next(errorHandler(400, "All fields are required"));
+    return next(errorHandler(400, "All fields are required"));
   }
 
   try {
@@ -48,26 +48,61 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) {
       return next(errorHandler(400, "invalid creditials, uh oh.. "));
-    } 
+    }
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SHH);
 
     const { password: pass, ...rest } = validUser._doc;
 
-
     res
       .status(200)
-      .cookie("access_token", token, {     
-         httpOnly: true,
-      }) 
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
       .json(rest);
-    
-
-  
-  
   } catch (error) {
     next(error);
   }
+};
 
+export const google = async (req, res, next) => {
+  const { name, email, googlePhotoUrl } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SHH);
+      const { password, ...rest } = user._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          name.toLowerCase().split(" ").join("") +
+          Math.random().toString(9).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture: googlePhotoUrl,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SHH);
+      const { password, ...rest } = newUser._doc;
+      res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+        })
+        .json(rest);
 
+    }
+  } catch (error) {
+    next(error);
+  }
 };
