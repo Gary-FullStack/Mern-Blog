4146ecb0f142dfe35bcd271d9bd699d645490674
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2024-01-18 18:23:28 -0500

    add private route
    put dashboard inside a private route

---
+++ client-side/src/components/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+// import { useSelector } from "react-redux";
+// import { Outlet, Navigate } from "react-router-dom";
+
+// export default function PrivateRoute() {
+
+//     const {currentUser} = useSelector((state) => state.user);
+//     return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+//   }
+
+
+import { useSelector } from 'react-redux';
+import { Outlet, Navigate } from 'react-router-dom';
+
+export default function PrivateRoute() {
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to='/sign-in' />;
+}
