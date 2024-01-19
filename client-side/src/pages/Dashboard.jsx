2d209397fb69685c2059bc904a34325f2872d3db
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2024-01-19 18:08:13 -0500

    user dashboard
    debugging and dashboard

--- client-side/src/pages/Dashboard.jsx
+++ client-side/src/pages/Dashboard.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import UserSidebar from '../components/UserSidebar';
+import UserProfile from '../components/UserProfile';
 
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
   return (
-    <div>Dashboard</div>
-  )
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
+        <UserSidebar />
+      </div>
+      {/* profile... */}
+      {tab === 'profile' && <UserProfile />}
+   
+    </div>
+  );
 }
