34be06d4df9fdef5f701e4edf0dc347f1612b220
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2024-01-21 13:29:40 -0500

    profile stuff
    too many errors to deal with right now

--- client-side/src/components/UserProfile.jsx
+++ client-side/src/components/UserProfile.jsx
@@ -1,56 +1,134 @@
-import { Button, TextInput } from 'flowbite-react';
-import { useEffect, useRef, useState } from 'react';
-import { useSelector } from 'react-redux';
-
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function UserProfile() {
-    const {currentUser} = useSelector((state) => state.user);
-    const [imageFile, setImageFile] = useState(null);
-    const [imageUrl, setImageUrl] = useState(null);
-    const filePickerRef = useRef();
+  const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [imageLoadProgress, setImageLoadProgress] = useState(null);
+  const [uploadError, setUploadError] = useState(null);
+  const filePickerRef = useRef();
 
-    const handleImage = (e) => {
-        const file = e.target.files[0];
-        if (file){
-            setImageFile(e.target.files[0]);
-            setImageUrl(URL.createObjectURL(file));
-        }       
-    };
-    useEffect(() => {
-        if (imageFile){
-            uploadImage();
-        }
-    }, [imageFile]);
+  const handleImage = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(e.target.files[0]);
+      setImageUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
 
-    const uploadImage = async () => {
-        console.log('loading...');
-    };
+  const uploadImage = async () => {
+    setUploadError(null);
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        //progress
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageLoadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setUploadError("failure during the upload (file must be less than 2Mb");
+      },
+      setImageLoadProgress(null),
+      setImageFile(null),
+      setImageUrl(null),
+      () => {
+        //success
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageUrl(downloadURL);
+        });
+      }
+    );
+  };
 
   return (
-    <div className='max-w-lg mx-auto p-3 w-full'>
-        <h1 className='my-7 text-center font-semibold text-3xl'>My Profile</h1>
-        <form className='flex flex-col gap-4'>
-            <input type='file' accept='image/*' onChange={handleImage} ref={filePickerRef} className='hidden'/>
-            <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' 
-            onClick={() => filePickerRef.current.click()}>
-                <img src={ imageUrl || currentUser.profilePicture} 
-                alt="user avatar" 
-                className='rounded-full w-full h-full object-cover border-8 border-[gray]'/>
-            </div>
-            <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
-            
-            <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email} />
-
-            <TextInput type='password' id='password' placeholder='password'/>
-
-            <Button type='submit' gradientDuoTone='purpleToBlue' outline>Update</Button>
-        </form>
-
-        <div className='text-red-500 flex justify-between mt-5'>
-            <span className='cursor-pointer'>Delete Account</span>
-            <span className='cursor-pointer'>Sign Out</span>
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="my-7 text-center font-semibold text-3xl">My Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImage}
+          ref={filePickerRef}
+          className="hidden"
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => filePickerRef.current.click()}
+        >
+          {imageLoadProgress && (
+            <CircularProgressbar
+              value={imageLoadProgress || 0}
+              text={`&{imageLoadProgres}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${imageLoadProgress / 100})`,
+                },
+              }}
+            />
+          )}
+          <img
+            src={imageUrl || currentUser.profilePicture}
+            alt="user avatar"
+            className={`rounded-full w-full h-full object-cover border-8 border-[gray]
+            &{imageLoadProgress && imageLoadProgress < 100 && 'opacity-60' }`}
+          />
         </div>
+        {uploadError && <Alert color="failure">{uploadError}</Alert>}
 
+        <TextInput
+          type="text"
+          id="username"
+          placeholder="username"
+          defaultValue={currentUser.username}
+        />
+
+        <TextInput
+          type="email"
+          id="email"
+          placeholder="email"
+          defaultValue={currentUser.email}
+        />
+
+        <TextInput type="password" id="password" placeholder="password" />
+
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
+        </Button>
+      </form>
+
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
+      </div>
     </div>
-  )
-} 
+  );
+}
--- client-side/src/components/UserSidebar.jsx
+++ client-side/src/components/UserSidebar.jsx
@@ -42,5 +42,5 @@ export default function UserSidebar() {
       </Sidebar.Items>
     </Sidebar>
   );
-}
+} 
  
