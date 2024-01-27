9ed3d5cdd114c9441dcfbbac954be4260b4879a5
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2024-01-27 17:36:22 -0500

    post create api
    start

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
@@ -21,12 +22,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.listen(3000, () => {
   console.log("Yo, Server is running on port 3000");
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
 
 // middleware
 app.use((err, req, res, next) => {
